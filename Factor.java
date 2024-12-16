557330a45a39b7650960e5e9ff10771a02a25cf3
adithya <adithya123@gmail.com>
2024-12-16 14:49:51 +0530

    Factors of java

---
+++ Factor.java
@@ -0,0 +1,11 @@
+public class factorial{
+	public static void main (String{}args){
+		int number=4;
+		System.out.print("Factors of "+ number +" are");
+		for(int i=1;i<=number;i++){
+			if(number%i==0){
+				System.out.print(i+" "):
+			}
+		}
+	}
+
