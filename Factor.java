545cbd5ae528a44df311dd94ee0bb9c55fdd6d88
Adithya-304 <adithyaadulapuram123@gmail.com>
2024-12-16 15:02:34 +0530

    Update Factor.java

--- Factor.java
+++ Factor.java
@@ -4,7 +4,7 @@ public class factorial{
 		System.out.print("Factors of "+ number +" are");
 		for(int i=1;i<=number;i++){
 			if(number%i==0){
-				System.out.print(i+" "):
+				System.out.print(i+" ");
 			}
 		}
 	}
